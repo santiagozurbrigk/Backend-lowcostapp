6f05e434da431ddebac2d3aad7d8aad341841742
santiagozurbrigk <santiagozurbrigkwork@gmail.com>
2025-06-29 23:50:45 -0300

    Fix memory storage S3 para pedidos

--- middleware/uploadMiddleware.js
+++ middleware/uploadMiddleware.js
@@ -1,14 +1,7 @@
 import multer from 'multer';
 import path from 'path';
 
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, './uploads/');
-    },
-    filename: function (req, file, cb) {
-        cb(null, `${Date.now()}-${file.originalname}`);
-    }
-});
+const storage = multer.memoryStorage();
 
 const fileFilter = (req, file, cb) => {
     const filetypes = /pdf/;
