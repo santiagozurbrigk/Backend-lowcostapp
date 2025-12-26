79f7944bcd0f5e0e2a2e47d261afa30fe4fef101
santiagozurbrigk <santiagozurbrigkwork@gmail.com>
2025-12-26 02:27:59 -0300

    Fix: Corregir nombre del archivo usuario.controller.js en import

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import { login, registro, renovarToken } from '../controllers/auth.controller.js';
 import authMiddleware from '../middleware/auth.middleware.js';
-import { perfil, actualizarPerfil } from '../controllers/usuarioController.js';
+import { perfil, actualizarPerfil } from '../controllers/usuario.controller.js';
 
 const router = express.Router();
 
