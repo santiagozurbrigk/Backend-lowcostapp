e51fa83cbe460687347b0b9b41801796f2e88899
santiagozurbrigk <santiagozurbrigkwork@gmail.com>
2025-12-26 02:15:38 -0300

    Configurar CORS para permitir requests desde frontend en Vercel

--- index.js
+++ index.js
@@ -24,7 +24,8 @@ app.use(cors({
         'http://localhost:5173',
         'http://localhost:3000',
         'http://lowcostimpresiones.com',
-        'https://lowcostimpresiones.com'
+        'https://lowcostimpresiones.com',
+        'https://frontend-pdfs.vercel.app' // Frontend en Vercel
     ],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
